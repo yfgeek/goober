3edef633c80af5a3452efdea8ba19cf489b2b33a
yfgeek <yifanb@gmail.com>
2019-04-21 00:06:14 +0800

    UTS Clone

---
+++ core/uts.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	"os/exec"
+	"syscall"
+	"os"
+	"log"
+)
+
+func main(){
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil{
+		log.Fatal(err)
+	}
+
+}
